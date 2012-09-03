cdd2d713f6057cae8f66f91dd2d960343074baa8
Mike Marsden <marsdems@muohio.edu>
2012-09-03 11:47:31 -0400

    Laying out the basic framework

--- src/Homework01App.cpp
+++ src/Homework01App.cpp
@@ -1,3 +1,19 @@
+/**
+ * @file Homework01App.cpp
+ * CSE 274 - Fall 2012
+ * My solution for HW01 Phase 01.
+ *
+ * @author Mike Marsden
+ * @date 2012-09-01
+ *
+ * @note This file is (c) 2012. It is licensed under the 
+ * CC BY 3.0 license (http://creativecommons.org/licenses/by/3.0/),
+ * which means you are free to use, share, and remix it as long as you
+ * give attribution. Commercial uses are allowed.
+ *
+ * @note This project satisfies goals A.1 (rectangle), A.2 (circle), B.1 (blur), E.2 (transparency),
+ * E.5 (animation) and E.6 (mouse interaction)
+ */
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Texture.h"
@@ -15,10 +31,24 @@ class Homework01App : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+
+  private:
+	Surface* mySurface_;
+
+	int frame_number_;
+
+	//Width and height of the screen
+	static const int kAppWidth=800;
+	static const int kAppHeight=600;
+	static const int kTextureSize=1024;
 };
 
 void Homework01App::setup()
 {
+	frame_number_=0;
+	
+	//This is the setup that everyone needs to do
+	mySurface_ = new Surface(kTextureSize,kTextureSize,false);
 }
 
 void Homework01App::mouseDown( MouseEvent event )
@@ -27,12 +57,20 @@ void Homework01App::mouseDown( MouseEvent event )
 
 void Homework01App::update()
 {
+	//Get our array of pixel information
+	uint8_t* dataArray = (*mySurface_).getData();
+
+	//Only save the first frame of drawing as output, code snippet via Dr. Brinkman
+	if(frame_number_ == 0){
+		writeImage("marsdems.png",*mySurface_);
+	}
+	//keeps track of how many frames we have shown.
+	frame_number_++;
 }
 
 void Homework01App::draw()
 {
-	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::draw(*mySurface_); 
 }
 
 CINDER_APP_BASIC( Homework01App, RendererGl )
